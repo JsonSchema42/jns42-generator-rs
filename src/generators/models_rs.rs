9355407b5a42a10162dbd7c42c55afded4ecd3e6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-04-08 16:09:55 +0200

    format tokens

--- src/generators/models_rs.rs
+++ src/generators/models_rs.rs
@@ -1,3 +1,6 @@
+use quote::{format_ident, quote, TokenStreamExt};
+use rust_format::Formatter;
+
 use crate::schemas::LoaderContext;
 
 pub struct ModelsRsGenerator<'a> {
@@ -9,5 +12,35 @@ impl<'a> ModelsRsGenerator<'a> {
         Self {
             schema_loader: loader,
         }
+    }
+
+    pub fn generate_file_content(&self) -> Result<String, &'static str> {
+        let mut tokens = quote! {};
+
+        {
+            let name = format_ident!("Model1");
+            tokens.append_all(quote! {
+                struct #name{
+
+                }
+            });
+        }
+
+        {
+            let name = format_ident!("Model2");
+            tokens.append_all(quote! {
+                struct #name{
+
+                }
+            });
+        }
+
+        let formatter = rust_format::RustFmt::new();
+
+        let content = formatter
+            .format_tokens(tokens)
+            .or(Err("error while formatting tokens"))?;
+
+        Ok(content)
     }
 }
--- src/generators/package.rs
+++ src/generators/package.rs
@@ -4,7 +4,6 @@ use std::{fs, path::PathBuf};
 use super::{cargo_toml::CargoTomlGenerator, models_rs::ModelsRsGenerator};
 
 pub struct PackageGenerator<'a> {
-    schema_loader: &'a LoaderContext<'a>,
     cargo_toml_generator: CargoTomlGenerator<'a>,
     models_rs_generator: ModelsRsGenerator<'a>,
 }
@@ -12,7 +11,6 @@ pub struct PackageGenerator<'a> {
 impl<'a> PackageGenerator<'a> {
     pub fn new(schema_loader: &'a LoaderContext<'a>) -> Self {
         Self {
-            schema_loader,
             cargo_toml_generator: CargoTomlGenerator::new(schema_loader),
             models_rs_generator: ModelsRsGenerator::new(schema_loader),
         }
@@ -26,12 +24,21 @@ impl<'a> PackageGenerator<'a> {
     ) -> Result<(), &'static str> {
         fs::create_dir_all(package_directory).or(Err("create directory failed"))?;
 
-        let content = self
-            .cargo_toml_generator
-            .generate_file_content(package_name, package_version)?;
+        {
+            let content = self
+                .cargo_toml_generator
+                .generate_file_content(package_name, package_version)?;
 
-        fs::write(package_directory.join("Cargo.toml"), content)
-            .or(Err("write Cargo.toml fails"))?;
+            fs::write(package_directory.join("Cargo.toml"), content)
+                .or(Err("write Cargo.toml fails"))?;
+        }
+
+        {
+            let content = self.models_rs_generator.generate_file_content()?;
+
+            fs::write(package_directory.join("models.rs"), content)
+                .or(Err("write models.rs fails"))?;
+        }
 
         Ok(())
     }
